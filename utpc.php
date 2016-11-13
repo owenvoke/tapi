661dac1b73a7d15b447178e4de980773da07fa3a
PXgamer <owzie123@gmail.com>
2016-11-13 14:30:54 +0000

    Added download() function to allow downloading of torrents via the API)

--- utpc.php
+++ utpc.php
@@ -3,6 +3,7 @@
 namespace pxgamer {
     class UTPC
     {
+		public static $api_endpoint = 'https://torrentapi.pxgamer.xyz/';
         public function __construct()
         {
         }
@@ -10,7 +11,6 @@ namespace pxgamer {
         public static function upload($api_key, $file_path = null)
         {
             if ($file_path !== '' && $file_path !== null) {
-                $api_endpoint = 'https://torrentapi.pxgamer.xyz/';
                 $postData = [
                     'api_key' => $api_key,
                     'torrent_file' => '@'.$file_path,
@@ -21,7 +21,7 @@ namespace pxgamer {
                 curl_setopt_array(
                     $ch,
                     [
-                        CURLOPT_URL => $api_endpoint,
+                        CURLOPT_URL => self::$api_endpoint,
                         CURLOPT_POST => 1,
                         CURLOPT_POSTFIELDS => $postData,
                         CURLOPT_SSL_VERIFYPEER => 0,
@@ -37,6 +37,30 @@ namespace pxgamer {
                 return json_encode(['status' => 'Please include a file path.']);
             }
         }
+		
+		public static function download($api_key, $hash)
+        {
+            echo self::get($api_key, 'f', $hash);
+        }
+		
+		private static function get($api_key, $mode = '', $hash = '')
+		{
+			$url = self::$api_endpoint . '?api_key=' . $api_key . '&mode=' . $mode . '&id=' . $hash;
+			$ch = curl_init();
+            curl_setopt_array(
+                $ch,
+                [
+                    CURLOPT_URL => $url,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_RETURNTRANSFER => 1,
+                ]
+            );
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            return $result;
+		}
     }
 
     $uTP = new \pxgamer\UTPC();
